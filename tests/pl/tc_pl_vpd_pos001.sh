923b08198d6bbbea5a042d8d646eaed04d4ef520
linuxfhy <linuxfhy@163.com>
2017-05-09 16:37:33 +0800

    update

---
+++ tests/pl/tc_pl_vpd_pos001.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+#
+# Copyright (c) 2017, Inspur. All rights reserved.
+#
+
+################################################################################
+#
+# __stc_assertion_start
+#
+# ID:	pl/tc_pl_vpd_pos001
+#
+# DESCRIPTION:
+#	Test cases for VPD-access
+#
+# __stc_assertion_end
+#
+################################################################################
+
+NAME=$(basename $0)
+CDIR=$(dirname  $0)
+TMPDIR=${TMPDIR:-"/tmp"}
+TSROOT=$CDIR/../../
+
+source $TSROOT/lib/libstr.sh
+source $TSROOT/lib/libcommon.sh
+source $TSROOT/config.vars
+
+S_SAINFO=/compass/bin/sainfo
+S_SATASK=/compass/bin/satask
+S_SVCTASK=/compass/bin/svctask
+S_EC_CHVPD=/compass/ec_chvpd
+
+tc_start $0
+trap "tc_xres \$?" EXIT
+
+exit $STF_PASS
