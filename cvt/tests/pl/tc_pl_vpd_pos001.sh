1edd4582b3b8091308b06792a29eb2d54e562770
linuxfhy <linuxfhy@163.com>
2017-05-29 10:09:41 +0800

    update

--- cvt/tests/pl/tc_pl_vpd_pos001.sh
+++ cvt/tests/pl/tc_pl_vpd_pos001.sh
@@ -123,25 +123,25 @@ kill_node -f >null 2>&1
 
 echo ""
 log ">>>>>>test case 1.1 start:write mid vpd use ipmi<<<<<<"
-#test_case_fun_1_1 write_midplanevpd_optimized_anyCPUcnt.sh
+test_case_fun_1_1 write_midplanevpd_optimized_anyCPUcnt.sh
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 1.1 pass<<<<<<"
 
 echo ""
 log ">>>>>>test case 1.2 start:write mid vpd use ec_chvpd<<<<<<"
-#test_case_fun_1_1 write_midplanevpd_use_ecchvpd.sh
+test_case_fun_1_1 write_midplanevpd_use_ecchvpd.sh
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 1.2 pass<<<<<<"
 
 echo ""
 log ">>>>>>test case 1.3 start:write can vpd use ipmi<<<<<<"
-#test_case_fun_1_1 write_canistervpd_optimized.sh
+test_case_fun_1_1 write_canistervpd_optimized.sh
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 1.3 pass<<<<<<"
 
 echo ""
 log ">>>>>>test case 1.4 start:write and read can vpd use ec_chvpd<<<<<<"
-#test_case_fun_1_1 write_canistervpd_use_ecchvpd.sh
+test_case_fun_1_1 write_canistervpd_use_ecchvpd.sh
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 1.4 pass<<<<<<"
 
@@ -149,7 +149,7 @@ echo ""
 log ">>>>>>test case 2.1 start:Test mid VPD access while both CMC ok<<<<<<"
 ifconfig eth2 up
 ifconfig eth3 up
-#test_case_fun_1_1 write_midplanevpd_use_ecchvpd.sh
+test_case_fun_1_1 write_midplanevpd_use_ecchvpd.sh
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 2.1 pass<<<<<<"
 
@@ -174,7 +174,7 @@ function test_case_fun_2_2()
 
 echo ""
 log ">>>>>>test case 2.2 start: Write by CMC0 and read by CMC1<<<<<<"
-#test_case_fun_2_2 w_0_r_1
+test_case_fun_2_2 w_0_r_1
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 2.2 pass<<<<<<"
 
@@ -185,7 +185,7 @@ function test_case_fun_2_3()
 }
 echo ""
 log ">>>>>>test case 2.3 start: Write one mid vpd and check other VPD is changed<<<<<<"
-#test_case_fun_2_3
+test_case_fun_2_3
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 2.3 pass<<<<<<"
 
@@ -214,13 +214,13 @@ function test_case_fun_2_5()
 
 echo ""
 log ">>>>>>test case 2.5 start: Change CMC0 to slave and test VPD access<<<<<<"
-#test_case_fun_2_5
+test_case_fun_2_5
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 2.5 pass<<<<<<"
 
 echo ""
 log ">>>>>>test case 2.6 start: Write, then change CMC0 to slave, then read<<<<<<"
-#test_case_fun_2_2 w_m_r_s
+test_case_fun_2_2 w_m_r_s
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 2.6 pass<<<<<<"
 
@@ -251,13 +251,13 @@ function test_case_fun_2_7()
 
 echo ""
 log ">>>>>>test case 2.7 start: Simulate CMC0 fail<<<<<<"
-#test_case_fun_2_7 eth2
+test_case_fun_2_7 eth2
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 2.7 pass<<<<<<"
 
 echo ""
 log ">>>>>>test case 2.8 start: Simulate CMC1 fail<<<<<<"
-#test_case_fun_2_7 eth3
+test_case_fun_2_7 eth3
 [[ $? == 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 2.8 pass<<<<<<"
 
@@ -275,7 +275,7 @@ function test_case_fun_3_2()
 }
 echo ""
 log ">>>>>>test case 3.2 start: Write one can vpd and check other VPD is changed<<<<<<"
-#test_case_fun_3_2
+test_case_fun_3_2
 [[ $? -eq 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 3.2 pass<<<<<<"
 
@@ -312,12 +312,12 @@ function test_case_fun_4_1()
      return $cmd_rc
 }
 log ">>>>>>test case 4.1 start: Start compass when cmc0 is master<<<<<<"
-#test_case_fun_4_1 1
+test_case_fun_4_1 1
 [[ $? -eq 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 4.1 pass<<<<<<"
 
 log ">>>>>>test case 4.2 start: Start compass when cmc0 is slave<<<<<<"
-#test_case_fun_4_1 0
+test_case_fun_4_1 0
 [[ $? -eq 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 4.2 pass<<<<<<"
 
@@ -331,12 +331,12 @@ function test_case_fun_4_3()
      return $cmd_rc
 }
 log ">>>>>>test case 4.3 start: Start compass when network to cmc0 is down<<<<<<"
-#test_case_fun_4_3 eth2
+test_case_fun_4_3 eth2
 [[ $? -eq 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 4.3 pass<<<<<<"
 
 log ">>>>>>test case 4.2 start: Start compass when network to cmc1 is down<<<<<<"
-#test_case_fun_4_3 eth3
+test_case_fun_4_3 eth3
 [[ $? -eq 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 4.2 pass<<<<<<"
 
@@ -349,12 +349,12 @@ function test_case_fun_6_1()
     sh write_midplanevpd_use_ecchvpd_inject_err.sh $1
 }
 log ">>>>>>test case 6.1 start: Inject timeout error once for each command<<<<<<"
-#test_case_fun_6_1 timeout
+test_case_fun_6_1 timeout
 [[ $? -eq 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 6.1 pass <<<<<<"
 
 log ">>>>>>test case 6.2 start: Inject result-short error once for each command<<<<<<"
-#test_case_fun_6_1 short
+test_case_fun_6_1 short
 [[ $? -eq 0 ]] || exit $STF_FAIL
 log ">>>>>>test case 6.2 pass <<<<<<"
 
